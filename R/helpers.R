c620d1ab35959a283def226bad862ae31244170d
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-02-16 16:27:55 +0100

    Warn about numeric NA when writing dbf file

--- R/helpers.R
+++ R/helpers.R
@@ -156,6 +156,24 @@ run_abimo_command_line <- function(args, tag = latest_abimo_version())
 #' @export
 write.dbf.abimo <- function (df_name, new_dbf)
 {
+  na_counts <- sapply(df_name, kwb.utils::nNA)
+  is_double <- sapply(df_name, is.double)
+
+  may_cause_warnings <- is_double & (na_counts > 0L)
+
+  if (any(may_cause_warnings)) {
+    message(
+      "foreign::write.dbf() may cause warnings due to NA values in the ",
+      "following numeric columns: ",
+      paste(collapse = ", ", sprintf(
+        "\"%s\" (%d-times)",
+        names(which(may_cause_warnings)),
+        na_counts[may_cause_warnings]
+      ))
+    )
+  }
+
   foreign::write.dbf(df_name, new_dbf)
+
   appendSubToFile(new_dbf)
 }
